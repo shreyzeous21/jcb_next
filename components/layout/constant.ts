a0de4b8de70afb679c3e125dc3d8b5bd71cee4cd
shreyzeous21 <shrey.sadhukhan21@gmail.com>
2026-02-04 23:00:57 +0530

    done

--- components/layout/constant.ts
+++ components/layout/constant.ts
@@ -51,27 +51,27 @@ export const benefits = [
     },
     {
         title: "SUPPORT AND TRAINING",
-        description: "Helps to attract customers and build trust in your business.",
+        description: "Help them run their business more efficiently and effectively.",
         Icon: FaHandsHelping,
     },
     {
         title: "MARKETING AND ADVERTISING",
-        description: "Helps to attract customers and build trust in your business.",
+        description: "Attract customers, which can help you attract new business and grow your customer base.",
         Icon: FaBullhorn,
     },
     {
         title: "EXCLUSIVE TERRITORIES",
-        description: "Helps to attract customers and build trust in your business.",
+        description: "Franchise agreements often come with exclusive territories, which can help ensure that you are not competing with other franchisees for the same customers",
         Icon: FaMapMarkedAlt,
     },
     {
         title: "GROUP PURCHASING POWER",
-        description: "Helps to attract customers and build trust in your business.",
+        description: "As part of a franchise, you can often take advantage of group purchasing power to negotiate better deals on supplies and equipment, which can help reduce your operating costs.",
         Icon: FaUsers,
     },
     {
         title: "REDUCED RISK",
-        description: "Helps to attract customers and build trust in your business.",
+        description: "Franchises often have a lower risk of failure compared to independent businesses because of the proven business model and support provided by the franchisor.",
         Icon: FaShieldAlt,
     },
 ]
